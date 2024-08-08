1dc16f6cfe6ac5e868f781c45dc77d9feadea473
vincelabutay <vincelabutay@gmail.com>
2024-08-08 23:27:06 +0800

    done todolist

--- app.js
+++ app.js
@@ -1,22 +1,56 @@
-const express =  require("express");
-const http = require ("https");
-const bodypParser = require("body-parser");
+const express = require("express");
+const http = require("https");
+
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
+const router = express.Router();
 
-app.get("/", function(req, res){
+var items = ["buy food", "cook food", "eat food"];
+let workItems = [];
 
-    var today = new Date();
+app.set("view engine", "ejs");
 
-    if (today.getDay() === 3){
-        res.send("Yay, its the weekend!");
-    } else {
-        res.send("boo! I have to work!");
-    }
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  let day = date.getDate();
+
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
 
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+  // items.push(item);
 
-app.listen(3000, function() {
-    console.log("Server is running on port 3000");
+  // res.redirect("/");
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
